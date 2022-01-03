9529a4e169b66e4a8995478255c6bab560ab914d
VictorGulart <victorlgulart@gmail.com>
2022-01-03 10:08:43 +0000

    added the heroku app url to the cors allowed

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -163,6 +163,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    'https://victor-todoapp.herokuapp.com',
 ]
 
 django_heroku.settings(locals())
