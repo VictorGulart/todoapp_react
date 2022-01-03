31d09571619e62b74b36644f7d4a684b625ea48c
VictorGulart <victorlgulart@gmail.com>
2022-01-03 07:21:32 +0000

    changes the staticfiles_dirs settings to include nothing

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -149,7 +149,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     # BASE_DIR / 'frontend/static',
-    os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
